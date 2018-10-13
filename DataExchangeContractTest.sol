7835d743bf77b3d143dad8140ced2dfbed3a2a76
zhangyaocheng <910486122@qq.com>
2018-10-13 14:04:13 +0800

    增加DataExchangeContractTest.sol 文件

---
+++ DataExchangeContractTest.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.4.18;
+
+
+contract DataExchangeContractTest {
+    
+
+    address public owner;
+    event addDataInfo(address addr,uint price, string datahash,string description);
+    event buyDataInfo(address buyer,uint price, string datahash, string description);
+
+    // 构建大数据模块的数据结构
+    struct DataInfo {
+        uint price; // 数据的定价信息
+        string datahash; // 数据在ipfs网络上的存储信息
+        string description; //数据的描述信息
+
+    }
+
+
+    // 映射 用户:大数据信息
+    mapping(address => DataInfo[]) public DataOwners;
+    // 映射 购买者:大数据信息
+    mapping(address => DataInfo[]) public Databuyers;
+    // 映射 用户:价值 反映用户有多少钱
+    mapping(address => uint) pendingWithdrawals; 
+
+    // 构造函数
+    constructor(){
+        owner = msg.sender;
+    }
+
+    // 注册函数
+    function register(uint price, string datahash, string description) public { 
+        DataOwners[msg.sender].push(
+            DataInfo({price:price,datahash:datahash,description:description})
+            );
+        emit addDataInfo(msg.sender,price,datahash,description);
+    }
+
+    // 购买数据函数
+    function Buy(address dataowneraddr, uint dataindex) public payable returns(string){
+        
+        require(DataOwners[dataowneraddr][dataindex].price <= msg.value,"你付的钱不足以购买该信息");
+        
+        pendingWithdrawals[dataowneraddr] += msg.value; // 存储账户价值
+        emit buyDataInfo(msg.sender,DataOwners[dataowneraddr][dataindex].price,DataOwners[dataowneraddr][dataindex].datahash,DataOwners[dataowneraddr][dataindex].description);
+        
+        return DataOwners[dataowneraddr][dataindex].datahash;
+
+    }
+
+    // 显示信息函数
+    function getDataPrice(address dataowneraddr, uint dataindex) public returns(uint){
+        var price = DataOwners[dataowneraddr][dataindex].price;
+        
+        return price;
+    }
+
+    function getDataDescription(address dataowneraddr, uint dataindex ) public returns(string){
+        var description = DataOwners[dataowneraddr][dataindex].description;
+        
+        return description;
+    }
+
+    // 使用withdraw方法使得数据拥有者受到数据的价值信息。避免用户阻塞buy函数导致合约不可用
+    function withdraw() public {
+
+        uint amount = pendingWithdrawals[msg.sender];
+        pendingWithdrawals[msg.sender]=0;
+        msg.sender.transfer(amount);
+        
+    }
+
+
+}
